bd40bb2d5bf5ef538d4482acfe3daddd6ed7749a
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-09-25 18:48:09 +0530

    navbar

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,11 +4,6 @@ function Home() {
   return (
     <div>
         <Navbar/>
-     <div className="container mt-5">
-  <h1 className="text-primary">Hello Bootstrap</h1>
- <button type="button" class="btn btn-danger">Danger</button>
-</div>
-
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,36 +1,43 @@
-import React from 'react'
 import { Link } from 'react-router-dom';
+
 function Navbar() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <div className="navbar-brand" href="#">E-Shop</div>
-          <button className='navbar-toggler' type='button' data-bs-toggle="collapse"></button>
-          <div className="collapse navbar-collapse" id='navbarsupportedContent'>
-            <ul className='navbar-nav me auto mb-2-lg-0'>
-              <li className='nav-item'>
-                <Link to='/'>Home</Link>
-              </li>
-              <li className='nav-item'>
-                <Link to='/'>Contact</Link>
-              </li> 
-              <li className='nav-item'>
-                <Link to='/'>About</Link>
-              </li>
-              <li className='nav-item'>
-                <Link to='/'>SignUp</Link>
-              </li>
-            </ul>
-            <form action="d-flex">
-              <input type="search" className='form-control me-2' placeholder='search' aria-label='Search' />
-            </form>
-          </div>
-        </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">E-Shop</Link>
 
-      </nav>
-    </div>
-  )
+     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+       <span className="navbar-toggler-icon"></span>
+     </button>
+     
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signup">SignUp</Link>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              type="search"
+              className="form-control me-2"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
