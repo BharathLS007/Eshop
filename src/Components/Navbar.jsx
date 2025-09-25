752c06a39f324ea41359fc737b4934772e39ac5f
BHARATH <120308878+BharathLS007@users.noreply.github.com>
2025-09-25 18:28:34 +0530

    nav design

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div>
-      
+      <div className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <div className="navbar-brand" href="#">E-Shop</div>
+          <button className='navbar-toggler' type='button' data-bs-toggle="collapse"></button>
+          <div className="collapse navbar-collapse" id='navbarsupportedContent'>
+            <ul className='navbar-nav me auto mb-2-lg-0'>
+              <li className='nav-item'>
+                <Link to='/'>Home</Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/'>Contact</Link>
+              </li> 
+              <li className='nav-item'>
+                <Link to='/'>About</Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/'>SignUp</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
